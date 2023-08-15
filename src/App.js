fd016e3b111ae0b933536b658cd279e3c25de5c2
Santiago Vargas <anglesla.santiagov@gmail.com>
2023-08-14 23:31:40 -0400

    Setting up initial layout of the page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <>
+        <section className="side-bar">
+          <button> + New Chat</button>
+          <ul className="chat-history"></ul>
+          <nav>
+            <p>AI Room!</p>
+          </nav>
+        </section>
+        <section className="main"></section>
+      </>
+    </div>
+  );
 }
 
 export default App;
